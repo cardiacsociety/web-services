bdc3465a4a9dcc0b5ff6d3641d142d1fda2534bc
Murugesan Muthusamy <murugesan.muthusamy@intergy.com.au>
2020-10-21 11:44:19 +0530

    Atlas db connection string

--- internal/platform/datastore/mongo.go
+++ internal/platform/datastore/mongo.go
@@ -37,7 +37,7 @@ func (mq MongoQuery) Do(c *mgo.Collection, r *[]interface{}) error {
 	return q.All(r)
 }
 
-// Connect to MongoDB
+// Connect to MongoDB - Connection string
 func (m *MongoDBConnection) Connect() error {
 	err := m.checkFields()
 	if err != nil {
