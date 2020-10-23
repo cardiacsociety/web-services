506143b9d739a975f1c459163fd070bd3b441366
Murugesan Muthusamy <murugesan.muthusamy@intergy.com.au>
2020-10-23 16:30:46 +0530

    couchbase path revert back

--- cmd/couchr/main.go
+++ cmd/couchr/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/34South/envr"
 	"github.com/cardiacsociety/web-services/internal/platform/datastore"
-	"github.com/couchbase/gocb/v2"
+	"gopkg.in/couchbase/gocb.v1"
 	"log"
 )
 
